2e1c2b6347c86b5a170a2be79bde56a1e5dbb2dc
Nyandika Donald <donald.nyandika@gmail.com>
2022-03-05 22:36:06 +0300

    Add html and styling files

--- app.tsx
+++ app.tsx
@@ -1,12 +1,30 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 
-export default function App({ Page, pageProps }: { Page: FC, pageProps: Record<string, unknown> }) {
+export default function App({
+  Page,
+  pageProps,
+}: {
+  Page: FC;
+  pageProps: Record<string, unknown>;
+}) {
   return (
     <main>
       <head>
-        <meta name="viewport" content="width=device-width" />
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+        <link rel='shortcut icon' href='favicon.png' />
+        <link rel='stylesheet' href='style/vendors/feather.css' />
+        <link rel='stylesheet' href='style/vendors/themify-icons.css' />
+        <link rel='stylesheet' href='style/vendors/vendor.bundle.base.css' />
+        <link rel='stylesheet' href='style/vendors/dataTables.bootstrap4.css' />
+        <link rel='stylesheet' href='style/vendors/themify-icons.css' />
+        <link rel='stylesheet' href='style/vendors/select.dataTables.min.css' />
+        <link rel='stylesheet' href='style/style.css'></link>
       </head>
       <Page {...pageProps} />
     </main>
-  )
+  );
 }
