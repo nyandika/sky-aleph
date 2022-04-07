f2008e35f880367fa0a96a35f97edc3a084b95e9
Nyandika Donald <nydonald@microsoft.com>
2022-04-07 16:04:15 +0300

    Take header and sidebar from index

--- app.tsx
+++ app.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import HeaderNavigation from "./components/navigation/header.tsx";
+import SideBar from "./components/navigation/sidebar.tsx";
 
 export default function App({
   Page,
@@ -10,21 +12,28 @@ export default function App({
   return (
     <main>
       <head>
-        <meta charSet='utf-8' />
+        <meta charSet="utf-8" />
         <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel='shortcut icon' href='favicon.png' />
-        <link rel='stylesheet' href='style/vendors/feather.css' />
-        <link rel='stylesheet' href='style/vendors/themify-icons.css' />
-        <link rel='stylesheet' href='style/vendors/vendor.bundle.base.css' />
-        <link rel='stylesheet' href='style/vendors/dataTables.bootstrap4.css' />
-        <link rel='stylesheet' href='style/vendors/themify-icons.css' />
-        <link rel='stylesheet' href='style/vendors/select.dataTables.min.css' />
-        <link rel='stylesheet' href='style/style.css'></link>
+        <link rel="shortcut icon" href="favicon.png" />
+        <link rel="stylesheet" href="style/vendors/feather.css" />
+        <link rel="stylesheet" href="style/vendors/themify-icons.css" />
+        <link rel="stylesheet" href="style/vendors/vendor.bundle.base.css" />
+        <link rel="stylesheet" href="style/vendors/dataTables.bootstrap4.css" />
+        <link rel="stylesheet" href="style/vendors/themify-icons.css" />
+        <link rel="stylesheet" href="style/vendors/select.dataTables.min.css" />
+        <link rel="stylesheet" href="style/style.css"></link>
+        <title>Skydash</title>
       </head>
-      <Page {...pageProps} />
+      <div className="container-scroller">
+        <div className="container-fluid page-body-wrapper">
+          <HeaderNavigation />
+          <SideBar />
+          <Page {...pageProps} />
+        </div>
+      </div>
     </main>
   );
 }
